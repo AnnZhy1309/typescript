8ba050cebbf7abecad4365af76bb6bfafba7c284
Anna Zhykol <annzhy1309@gmail.com>
2023-06-29 13:09:40 +0200

    ts

--- Make an interface generic/exercise-2-make-an-interface-generic-start.ts
+++ Make an interface generic/exercise-2-make-an-interface-generic-start.ts
@@ -6,19 +6,19 @@
 // This should fix the type errors on Lines 14 and 21.
 // Hint: The type for the `languages` property should use a type variable.
 
-interface Country {
-    name: string;
-    languages: unknown;
+interface Country<Type,Type2>{
+    name: Type;
+    languages: Type2;
 }
 
-const languagesObj1: Country<string> = {
+const languagesObj1: Country <string, string>= {
     name: "New Zealand",
     languages: "English, Māori"
 };
 
 console.log(languagesObj1.languages);
 
-const languagesObj2: Country<string[]> = {
+const languagesObj2: Country<string,string[]> = {
     name: "Spain",
     languages: ["Spanish", "Catalan", "Galician", "Basque", "Valencian"],
 };
